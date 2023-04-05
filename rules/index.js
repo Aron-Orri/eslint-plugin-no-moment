9ad6a51a89a6c2ef8925fa7138b9b9cf78f6747f
Aron-Orri <aron.orri@gmail.com>
2023-04-05 15:35:42 +0000

    No moment rule added

--- rules/index.js
+++ rules/index.js
@@ -1,9 +1,36 @@
 module.exports = {
     meta: {
-        name: "eslint-no-moment",
+        name: "no-moment",
         version: "0.0.1"
     },
     rules: {
+        "no-moment": {
+            meta: {
+                type: "problem",
+                docs: {
+                    description: "Eslint plugin that forbids moment library from being used"
+                },
+                messages: {
+                    errorMessage: "hello"
+                }
+            },
+            create: function (context) {
+                function createReport(node) {
+                    return context.report({
+                        node: node,
+                        meassageId: "errorMessage"
+                    })
+                }
 
+                return {
+                    "ImportDeclaration": (node) => {
+                        if (node.value == "moment") return createReport
+                    },
+                    "VariableDeclarator > CallExpression Literal": (node) => {
+                        if (node.value == "moment") return createReport
+                    }
+                }
+            }
+        }
     }
 };
